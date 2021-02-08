8813fd0d449850dbf5d01c274a641c85fbae4173
nigm4 <nigmasilmi.web@gmail.com>
2021-02-08 15:34:19 -0300

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCSp-jY0YiJ4TmSxwCjwqTYGUuFzc8CkNo";
+const KEY = [theKey];
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
